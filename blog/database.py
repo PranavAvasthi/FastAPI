3ebeacbd617807cea497326104d7088e8cb38d83
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-16 22:27:10 +0530

    Store Blog Details in DB

--- blog/database.py
+++ blog/database.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
 
-SessionLocal = sessionmaker(engine, autocommit=False, autoFlush=False)
+SessionLocal = sessionmaker(engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
