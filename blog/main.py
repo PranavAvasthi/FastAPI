48754bec06d0cd1b53f67fdba01234abf8d8b3ac
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-16 22:00:29 +0530

    Models and Tables Created

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI # type: ignore
-from . import schemas
+from . import schemas, models
+from .database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def create(request: schemas.Blog):
