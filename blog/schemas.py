fb8e0e03f8c728fe55d73575a5e2eff7c06ca6bb
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-18 11:54:11 +0530

    Response Model

--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel # type: ignore
 class Blog(BaseModel):
     title: str
     body: str
+    
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    class Config():
+        orm_mode = True
