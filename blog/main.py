a78b2b7001f3c6ee34c9b14b0f844e6d6a3d44f8
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-18 12:24:44 +0530

    Get User by id

--- blog/main.py
+++ blog/main.py
@@ -27,7 +27,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog:
+    if not blog.first():
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f'Blog with id {id} not found')
     blog.delete(synchronize_session=False)
     db.commit()
@@ -56,10 +56,17 @@ def show(id, response: Response, db: Session = Depends(get_db)):
         # return {'detail': f'Blog with the id {id} is not available'}
     return blog
 
-@app.post('/user', status_code=status.HTTP_201_CREATED)
+@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with the id {id} is not available')
+    return user
